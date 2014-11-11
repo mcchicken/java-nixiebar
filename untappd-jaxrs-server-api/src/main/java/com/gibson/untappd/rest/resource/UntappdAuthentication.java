e6639faba6355de65832627baa7c4b898d094209
Jeff Gibson <gibson.jeff@gmail.com>
2014-11-10 22:01:25 -0500

    Updated oauth setup

--- untappd-jaxrs-server-api/src/main/java/com/gibson/untappd/rest/resource/UntappdAuthentication.java
+++ untappd-jaxrs-server-api/src/main/java/com/gibson/untappd/rest/resource/UntappdAuthentication.java
@@ -3,6 +3,7 @@ package com.gibson.untappd.rest.resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import com.gibson.untappd.rest.domain.Token;
 
 @Path("oauth")
 public interface UntappdAuthentication {
