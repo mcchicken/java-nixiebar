ce1deacd0621a018f39a18e0e51293e99ac0d2b1
Jeff Gibson <gibson.jeff@gmail.com>
2014-12-02 00:26:49 -0500

    Added bootstrap theme "paper"

--- untappd-angular-ui/src/main/webapp/js/nixiebar.js
+++ untappd-angular-ui/src/main/webapp/js/nixiebar.js
@@ -1,5 +1,5 @@
 var nixieBar = angular.module('nixieBar',[]);
 
 nixieBar.controller('NixieBarController', ['$scope', function($scope) {
-  $scope.untappdUser = 'Enter your user name';
+	$scope.appName = 'NixieBar';
 }]);
