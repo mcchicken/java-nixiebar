678228f78223e388883a44d51a6dff72db07e811
Jeff Gibson <gibson.jeff@gmail.com>
2015-01-28 20:00:19 -0500

    Added APIs to support turning on and off the user information polling process

--- untappd-jaxrs-server-api/src/main/java/com/gibson/untappd/rest/resource/UntappdBarDisplayable.java
+++ untappd-jaxrs-server-api/src/main/java/com/gibson/untappd/rest/resource/UntappdBarDisplayable.java
@@ -3,4 +3,6 @@ package com.gibson.untappd.rest.resource;
 public interface UntappdBarDisplayable {
     int totalUniqueCheckinsFor(String username);
     int totalCheckinsFor(String username);
+    void turnOnDisplayCycleFor(String username, int delay);
+    void turnOffDisplayCycleFor(String username);
 }
