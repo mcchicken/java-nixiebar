2ec1494dbc406f5a7296e0db99402b60285245e2
Jeff Gibson <gibson.jeff@gmail.com>
2015-02-15 22:42:53 -0500

    Added logger and some debugging into client

--- untappd-jaxrs-client-resteasy-impl/src/main/java/com/gibson/untappd/rest/client/UntappdNixieBarClientImpl.java
+++ untappd-jaxrs-client-resteasy-impl/src/main/java/com/gibson/untappd/rest/client/UntappdNixieBarClientImpl.java
@@ -4,6 +4,8 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.gibson.untappd.rest.adapter.TokenGsonAdapter;
 import com.gibson.untappd.rest.domain.Client;
@@ -15,6 +17,7 @@ import com.gibson.untappd.rest.resource.UntappdUserStats;
 import com.google.common.base.Preconditions;
 
 public class UntappdNixieBarClientImpl {
+	private static final Logger logger = LoggerFactory.getLogger(UntappdNixieBarClientImpl.class);
 	private static final String CLIENT_ID = "D5F54706652DD942DCF1C6AC855B7C22CF810FAE";
 	private static final String CLIENT_SECRET = "D5F54706652DD942DCF1C6AC855B7C22CF810FAE";
 	private static final String REDIRECT_URL = "http://nixiebar.com:8080/untappd-jaxrs-server-resteasy-impl/rest/oauth";
@@ -32,6 +35,7 @@ public class UntappdNixieBarClientImpl {
 	}
 	
 	public Token getAccessToken(String code) {
+		logger.debug("Retrieving access token using code {}", code);
 		Preconditions.checkNotNull(code, "Can't retrieve access token without code");
 		
 		return authenticationHandler.getAccessTokenFromRedirect(CLIENT_ID, CLIENT_SECRET
@@ -39,11 +43,13 @@ public class UntappdNixieBarClientImpl {
 	}
 	
 	public int getTotalUniqueCheckinsFor(String username) {
+		logger.debug("Retrieving total beer checkins for {}", username);
 		User user = userStatsRetriever.getUserStats(username);
 		return user.getStats().getTotal_beers();
 	}
 	
 	private UntappdAuthenticationHandler createOAuthHandler() {
+		logger.debug("Building proxy client for OAuth authentication...");
 		WebTarget target = ClientBuilder
 						.newClient()
 						.register(GsonProvider.class)
@@ -54,6 +60,7 @@ public class UntappdNixieBarClientImpl {
 	}
 
 	private UntappdUserStats createUserStatsRetriever() {
+		logger.debug("Building proxy client for user stats...");
 		WebTarget target = ClientBuilder
 						.newClient()
 						.target(UntappdUserStats.USER_STATS_URL);
