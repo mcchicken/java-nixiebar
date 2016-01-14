468996084001f82a2fa9c05a40280e802dbab636
Jeff Gibson <gibson.jeff@gmail.com>
2016-01-13 21:55:55 -0500

    Updated swagger.json information

--- nixiebar-jersey-impl/src/main/java/com/nixiebar/rest/application/NixieBarRestImpl.java
+++ nixiebar-jersey-impl/src/main/java/com/nixiebar/rest/application/NixieBarRestImpl.java
@@ -3,6 +3,8 @@ package com.nixiebar.rest.application;
 import com.nixiebar.rest.impl.AuthenticationHandlerImpl;
 import com.nixiebar.rest.impl.CheckinStatsImpl;
 import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -13,13 +15,15 @@ import java.util.Set;
 public class NixieBarRestImpl extends Application {
 
     public NixieBarRestImpl() {
-//        BeanConfig beanConfig = new BeanConfig();
-//        beanConfig.setVersion("1.0.2");
-//        beanConfig.setSchemes(new String[]{"http"});
-//        beanConfig.setHost("localhost:8002");
-//        beanConfig.setBasePath("/api");
-//        beanConfig.setResourcePackage("io.swagger.resources");
-//        beanConfig.setScan(true);
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setTitle("NixieBar");
+        beanConfig.setDescription("Retrieve beer stats for display on nixie tubes.");
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/nixiebar-jersey-impl-1.0/rest");
+        beanConfig.setResourcePackage("com.nixiebar.rest.impl");
+        beanConfig.setScan(true);
     }
 
     @Override
@@ -29,8 +33,8 @@ public class NixieBarRestImpl extends Application {
         resources.add(AuthenticationHandlerImpl.class);
         resources.add(CheckinStatsImpl.class);
 
-//        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
-//        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
 
         return resources;
     }
